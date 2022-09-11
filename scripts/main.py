1d7484064d65fc17bbffe21ffbdf0d4162fc0ce1
Toshiya Nakakura <nakakura@me.com>
2022-09-11 09:51:05 +0200

    別プロセスから終了指示を受けるようにした

--- scripts/main.py
+++ scripts/main.py
@@ -3,14 +3,24 @@
 import rospy
 import signal
 import subprocess
+import threading
 from subprocess import PIPE
 import os
 import psutil
 
 from gstreamer_launcher.srv import *
 
+exiting_thread = None
+
+def exiting():
+    rospy.sleep(0.2)
+    rospy.signal_shutdown("finish")
+
+
 
 def launcher(req):
+    global exiting_thread
+
     if req.message_type == "LAUNCH":
         proc = subprocess.Popen(req.command, shell=True, restore_signals=True, stdout=PIPE, stderr=PIPE)
         try:
@@ -26,6 +36,11 @@ def launcher(req):
             return GStreamerLauncherResponse(False, -1)
 
         return GStreamerLauncherResponse(True, 0)
+    elif req.message_type == "SYSTEM_EXIT":
+        # メッセージだけ返したらexitする
+        exiting_thread = threading.Thread(target=exiting)
+        exiting_thread.start()
+        return GStreamerLauncherResponse(True, 0)
 
     return GStreamerLauncherResponse(False, -1)
 
@@ -36,14 +51,20 @@ def gst_launch_server():
 
 
 def sigterm_handler(signal, frame):
+    # SYSTEM_EXITを受け取らない限り何があっても落とさない
+    # 単独で起動することはせず、roslaunch内でclientと一緒に起動すべき
     pass
 
 
 def main():
+    global exiting_thread
     gst_launch_server()
+    if not exiting_thread is None:
+        exiting_thread.join()
 
 
 if __name__ == "__main__":
     rospy.init_node('gstreamer_launcher', log_level=rospy.INFO)
     signal.signal(signal.SIGTERM, sigterm_handler)
+    signal.signal(signal.SIGINT, sigterm_handler)
     main()
